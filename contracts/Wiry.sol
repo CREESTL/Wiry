7fd160e355c90ad0e795db5bd2c5713cbf9a13a3
Danila Egorov <degorov@sfxdx.com>
2023-05-05 18:43:50 +0000

    Add ERC20 token

---
+++ contracts/Wiry.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interfaces/IWiry.sol";
+
+/// @title The WIRY token
+contract Wiry is IWiry, ERC20, ERC20Burnable, Pausable, Ownable {
+
+	/// @notice The initial amount of tokens that should be minted
+	uint256 constant initialSupply = 100_000;
+
+	/// @notice List of users which are forbidden to receive or send tokens
+	mapping (address => bool) blackListed;
+
+    constructor() ERC20("Wiry", "WIRY") {
+    	// Mint initial supply to the owner
+        _mint(msg.sender, initialSupply * 10 ** decimals());
+    }
+
+
+    /// @notice See {IWiry-pause}
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    /// @notice See {IWiry-unpause}
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    /// @notice See {IWiry-mint}
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    /// @notice See {IWiry-burn}
+	function burn(address from, uint256 amount) external onlyOwner {
+		_burn(from, amount);
+	}
+
+	/// @notice See {IWiry-addToBlackList}
+	function addToBlackList(address user) external onlyOwner {
+		require(user != address(0), "Wiry: Zero address!");
+		require(!blackListed[user], "Wiry: Already blacklisted!");
+		blackListed[user] = true;
+		emit AddedToBlackList(user);
+	}
+
+	/// @notice See {IWiry-removeFromBlackList}
+	function removeFromBlackList(address user) external onlyOwner {
+		require(user != address(0), "Wiry: Zero address!");
+		require(blackListed[user], "Wiry: Not blacklisted!");
+		blackListed[user] = false;
+		emit RemovedFromBlackList(user);
+	}
+
+	/// @dev This hook is triggers before each token transfer. Allows
+	///      to run extra checks
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        // Check that contract is not paused
+        whenNotPaused
+        override
+    {
+    	// If either sender of receiver is blacklisted, cancel transfer
+    	require(!blackListed[from], "Wiry: Sender is blacklisted!");
+    	require(!blackListed[to], "Wiry: Receiver is blacklisted!");
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
