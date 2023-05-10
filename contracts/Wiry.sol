2ab19218f0106b20b4230cf503ea5394561be41c
Danila Egorov <degorov@sfxdx.com>
2023-05-10 08:34:53 +0000

    Add tests

--- contracts/Wiry.sol
+++ contracts/Wiry.sol
@@ -2,25 +2,23 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./interfaces/IWiry.sol";
 
 /// @title The WIRY token
-contract Wiry is IWiry, ERC20, ERC20Burnable, Pausable, Ownable {
+contract Wiry is IWiry, ERC20, Pausable, Ownable {
 
-	/// @notice The initial amount of tokens that should be minted
-	uint256 constant initialSupply = 100_000;
+    /// @notice The initial amount of tokens that should be minted
+    uint256 public constant INITIAL_SUPPLY = 100_000;
 
-	/// @notice List of users which are forbidden to receive or send tokens
-	mapping (address => bool) blackListed;
+    /// @notice List of users which are forbidden to receive or send tokens
+    mapping(address => bool) public blackListed;
 
     constructor() ERC20("Wiry", "WIRY") {
-    	// Mint initial supply to the owner
-        _mint(msg.sender, initialSupply * 10 ** decimals());
+        // Mint initial supply to the owner
+        _mint(msg.sender, INITIAL_SUPPLY * 10 ** decimals());
     }
-
 
     /// @notice See {IWiry-pause}
     function pause() external onlyOwner {
@@ -37,38 +35,46 @@ contract Wiry is IWiry, ERC20, ERC20Burnable, Pausable, Ownable {
         _mint(to, amount);
     }
 
-    /// @notice See {IWiry-burn}
-	function burn(address from, uint256 amount) external onlyOwner {
+	/// @notice See {IWiry-burnFrom}
+	function burnFrom(
+		address from,
+		uint256 amount
+		) external onlyOwner {
 		_burn(from, amount);
 	}
 
-	/// @notice See {IWiry-addToBlackList}
-	function addToBlackList(address user) external onlyOwner {
-		require(user != address(0), "Wiry: Zero address!");
-		require(!blackListed[user], "Wiry: Already blacklisted!");
-		blackListed[user] = true;
-		emit AddedToBlackList(user);
-	}
 
-	/// @notice See {IWiry-removeFromBlackList}
-	function removeFromBlackList(address user) external onlyOwner {
-		require(user != address(0), "Wiry: Zero address!");
-		require(blackListed[user], "Wiry: Not blacklisted!");
-		blackListed[user] = false;
-		emit RemovedFromBlackList(user);
-	}
+    /// @notice See {IWiry-addToBlackList}
+    function addToBlackList(address user) external onlyOwner {
+        require(user != address(0), "Wiry: Zero address!");
+        require(!blackListed[user], "Wiry: Already blacklisted!");
+        blackListed[user] = true;
+        emit AddedToBlackList(user);
+    }
 
-	/// @dev This hook is triggers before each token transfer. Allows
-	///      to run extra checks
-    function _beforeTokenTransfer(address from, address to, uint256 amount)
+    /// @notice See {IWiry-removeFromBlackList}
+    function removeFromBlackList(address user) external onlyOwner {
+        require(user != address(0), "Wiry: Zero address!");
+        require(blackListed[user], "Wiry: Not blacklisted!");
+        blackListed[user] = false;
+        emit RemovedFromBlackList(user);
+    }
+
+    /// @dev This hook is triggers before each token transfer. Allows
+    ///      to run extra checks
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    )
         internal
+        override
         // Check that contract is not paused
         whenNotPaused
-        override
     {
-    	// If either sender of receiver is blacklisted, cancel transfer
-    	require(!blackListed[from], "Wiry: Sender is blacklisted!");
-    	require(!blackListed[to], "Wiry: Receiver is blacklisted!");
+        // If either sender of receiver is blacklisted, cancel transfer
+        require(!blackListed[from], "Wiry: Sender is blacklisted!");
+        require(!blackListed[to], "Wiry: Receiver is blacklisted!");
         super._beforeTokenTransfer(from, to, amount);
     }
 }
